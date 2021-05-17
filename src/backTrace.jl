31b3656c4361c4f7600f540562b335d1265c0587
DanielRivasMD <danielrivasmd@gmail.com>
2021-05-17 07:29:18 +0200

    add backtrace

---
+++ src/backTrace.jl
@@ -0,0 +1,39 @@
+################################################################################
+
+""
+function backTrace(self::HMM)
+  state = 0
+  min = self.tbM[1][end]
+
+  for ix in eachindex(self.tbM)
+    if self.tbM[ix][end] <= min
+      state = ix
+      min = self.tbM[ix][end]
+    end
+
+    tb = fill(-1, size(self.tbM[1], 1))
+
+    for ix in reverse(eachindex(self.tbM[1]))
+      vlocal = min
+      global sig = -1
+
+      for jx in eachindex(self.tbM)
+        dob = self.tbM[jx][ix]
+        if dob <= vlocal
+          vlocal = dob
+          sig = jx
+        end
+      end
+
+      tb[ix] = state
+      state = sig
+
+    end
+
+    tb = tb[1:end - 1]
+
+    return tb
+  end
+end
+
+################################################################################
