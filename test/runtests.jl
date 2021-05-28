acfa5a942ec2e5955633e933936818ec10d4c65f
DanielRivasMD <danielrivasmd@gmail.com>
2021-05-28 12:39:24 +0200

    test controller

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,23 @@ using HiddenMarkovModelReaders
 using Test
 
 @testset "HiddenMarkovModelReaders.jl" begin
-    # Write your tests here.
+  @testset "Distances & Amplitude" begin
+    @test distance() == # Euclidean distance
+    @test bhattDist() == # Bhattacharyya distance
+    @test amplitude() == # amplitude
+  end
+
+  @testset "Setup Hidden Markov Model" begin
+    using DelimitedFiles
+    f = readdlm("signal.tsv")
+
+    @test setup(f) == (200., )
+
+  end
+
+  @testset "Process Hidden Markov Model" begin
+
+
+    @test process()
+  end
 end
