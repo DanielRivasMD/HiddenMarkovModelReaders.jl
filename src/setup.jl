c732aa5c09a6fae413e31142a6e88831fecc3dba
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-01 11:16:18 +0200

    patch missing type

--- src/setup.jl
+++ src/setup.jl
@@ -6,7 +6,7 @@
 
 Initiate hidden Markov model object
 """
-function setup!(v::Array{T, 2}; penalty = 200.) where <: Number
+function setup!(v::Array{T, 2}; penalty = 200.) where T <: Number
   # mPen = 200.
   noIter = 1
   hmm = HMM([zeros(size(v, 2)) for i in 1:noIter], [zeros(size(v, 1) + 1) for i in 1:noIter])
