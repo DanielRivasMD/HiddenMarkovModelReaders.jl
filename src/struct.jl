1247fb2e0ff0b8366d3ecb227a134bd8de5c0b62
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-01 11:16:29 +0200

    add struct annotations

--- src/struct.jl
+++ src/struct.jl
@@ -1,10 +1,28 @@
 ################################################################################
 
+"""
+Keep track of scores & indexes
+
+# Fields
+    score::Float64: score
+    index::Int64: index
+
+See also: [`HMM`](@ref)
+"""
 struct ScorePair
   score::Float64
   index::Int64
 end
 
+"""
+Hidden Markov model object
+
+# Fields
+    dataM::Array: hold data
+    tbM::Array: collect traceback
+
+See also: [`ScorePair`](@ref)
+"""
 mutable struct HMM
   dataM::Array
   tbM::Array
