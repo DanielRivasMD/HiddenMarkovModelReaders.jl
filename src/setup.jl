a6192086e5215c5eb13d6a735b06093e25345715
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-01 12:50:26 +0200

    segregate penalty from setup

--- src/setup.jl
+++ src/setup.jl
@@ -1,17 +1,15 @@
 ################################################################################
 
 """
-    setup(v::Array{T, 2};
-    penalty = 200.) where <: Number
+    setup(v::Array{T, 2}) where <: Number
 
 Initiate hidden Markov model object
 """
-function setup!(v::Array{T, 2}; penalty = 200.) where T <: Number
-  # mPen = 200.
+function setup!(v::Array{T, 2}) where T <: Number
   noIter = 1
   hmm = HMM([zeros(size(v, 2)) for i in 1:noIter], [zeros(size(v, 1) + 1) for i in 1:noIter])
   reset!(hmm)
-  return (penalty, hmm)
+  return hmm
 end
 
 ################################################################################
