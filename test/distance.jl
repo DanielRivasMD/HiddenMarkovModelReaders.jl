e5ce67bb0876643e72e59bb56bb2841c0cd7cdba
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-10 11:01:50 +0200

    test distance functions

--- test/distance.jl
+++ test/distance.jl
@@ -1,9 +1,15 @@
 ################################################################################
 
-@testset "Distances & Amplitude" begin
-  @test distance() == # Euclidean distance
-  @test bhattDist() == # Bhattacharyya distance
-  @test amplitude() == # amplitude
+@testset "Euclidean distance" begin
+  @test euclDist(collect(1:10), fill(5, 10)) == 9.219544457292887
+end
+
+@testset "Bhattacharyya distance" begin
+  @test bhattDist(collect(1:10), fill(5, 10)) == -3.936532135073928
+end
+
+@testset "Amplitude" begin
+  @test HiddenMarkovModelReaders.amplitude(collect(1:10)) == 19.621416870348583
 end
 
 ################################################################################
