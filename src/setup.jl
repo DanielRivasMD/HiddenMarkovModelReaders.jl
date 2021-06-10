966378bd5f86f84210bd7eb5fba435c30f8fe561
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-10 11:00:05 +0200

    reduce setup footprint

--- src/setup.jl
+++ src/setup.jl
@@ -8,8 +8,7 @@ Initiate hidden Markov model object
 See also: [`process!`](@ref), [`HMM`](@ref), [`HMMParams`](@ref)
 """
 function setup!(v::Array{T, 2}) where T <: Number
-  noIter = 1
-  hmm = HMM([zeros(size(v, 2)) for i in 1:noIter], [zeros(size(v, 1) + 1) for i in 1:noIter])
+  hmm = HMM([zeros(size(v, 2))], [zeros(size(v, 1) + 1)])
   reset!(hmm)
   return hmm
 end
