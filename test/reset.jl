1abbbc8e4b8aa5c8fc3137045267a8ce2f0c3b93
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-29 15:13:04 +0200

    add reset tests

--- test/reset.jl
+++ test/reset.jl
@@ -2,13 +2,33 @@
 
 @testset "Reset" begin
 
-  # read file
+  ################################################################################
+
+  # load small array into hidden Markov model
+  include("data/smallArray.jl")
+
+  # reset
+  resetedhmm = HiddenMarkovModelReaders.reset!(HMM([zeros(size(x, 2))], [zeros(size(x, 1) + 1)]))
+
+  # testing
+  @test resetedhmm == [[0.0; repeat([-1.0], size(x, 1))]]
+  @test hmm.dataM == [zeros(size(x, 2))]
+  @test hmm.tbM == [[0.0; repeat([-1.0], size(x, 1))]]
+
+  ################################################################################
+
+  # read file into hidden Markov model
   include("data/readfile.jl")
 
-  # setup hidden Markov model
+  # reset
   resetedhmm = HiddenMarkovModelReaders.reset!(HMM([zeros(size(v, 2))], [zeros(size(v, 1) + 1)]))
 
+  # testing
   @test resetedhmm == [[0.0; repeat([-1.0], size(v, 1))]]
+  @test hmm.dataM == [zeros(size(v, 2))]
+  @test hmm.tbM == [[0.0; repeat([-1.0], size(v, 1))]]
+
+  ################################################################################
 
 end
 
