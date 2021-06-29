add67f39469c42e1228735137c22ff00b344ca81
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-29 15:10:59 +0200

    patch mutating reset function signature

--- src/reset.jl
+++ src/reset.jl
@@ -3,7 +3,6 @@
 "Reset hidden Markov model object"
 function reset!(self::HMM)
   self.tbM = [x .= [0; repeat(-1:-1, length(self.tbM[1]) - 1)] for x in self.tbM]
-  return self.tbM
 end
 
 ################################################################################
