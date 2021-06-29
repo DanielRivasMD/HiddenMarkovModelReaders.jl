5a51e9420f9164db159d4c54e00d8ae5d374771a
DanielRivasMD <danielrivasmd@gmail.com>
2021-06-29 15:11:32 +0200

    additional setup functions tests

--- test/setup.jl
+++ test/setup.jl
@@ -2,11 +2,25 @@
 
 @testset "Setup" begin
 
+  ################################################################################
+
+  # load small array into hidden Markov model
+  include("data/smallArray.jl")
+
+  # testing
+  @test hmm.dataM == [zeros(size(x, 2))]
+  @test hmm.tbM == [[0.0; repeat([-1.0], size(x, 1))]]
+
+  ################################################################################
+
   # read file into hidden Markov model
   include("data/readfile.jl")
 
+  # testing
   @test hmm.dataM == [zeros(size(v, 2))]
   @test hmm.tbM == [[0.0; repeat([-1.0], size(v, 1))]]
+
+  ################################################################################
 
 end
 
